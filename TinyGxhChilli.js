bcca2e24e7581868377e8df29b09914786124fc7
ril3y <rileyporter@gmail.com>
2015-03-07 17:06:39 -0500

    Added new nodejs module for xhc

---
+++ TinyGxhChilli.js
@@ -0,0 +1,557 @@
+var HID = require("node-hid");
+var WebSocket = require('ws');
+var argv = require('optimist').argv;
+var Debug = require("console-debug");
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+
+var console = new Debug({
+    uncaughtExceptionCatch: false, // Do we want to catch uncaughtExceptions?
+    consoleFilter: [], // Filter these console output types, Examples: 'LOG', 'WARN', 'ERROR', 'DEBUG', 'INFO'
+    logToFile: true, // if true, will put console output in a log file folder called 'logs'
+    logFilter: ['LOG', 'DEBUG', 'INFO'], // Examples: Filter these types to not log to file, Examples: 'LOG', 'WARN', 'ERROR', 'DEBUG', 'INFO'
+    colors: true // do we want pretty pony colors in our console output?
+});
+
+
+var Xpen = function () {
+    EventEmitter.call(this);
+    isPaused = false;
+    isJogging = false;
+    stepDistance = 0;
+    distanceTable = [0.1, 0.01, 0.001];
+    jogMode = "incremental"; //vs "continuous"
+    velocityMin = 40; //mm/min
+    calculatedVelocity = velocityMin;
+
+
+
+    this.vendorId = 4302;
+    this.productId = 60272;
+
+
+    //=====================
+    // Defines
+    //=====================
+    this.INCHES = 0;
+    this.MM = 1;
+    this.units = this.MM;
+
+
+    //=====================
+    //CMD Byte Packet Names
+    //=====================
+    CMD_START_BYTE = 0;
+    CMD_BYTE1 = 1;
+    CMD_PADDING = 2;
+    CMD_DIAL_BYTE = 3;
+    CMD_VELOCITY = 4;
+    CMD_BYTE2 = 5;
+
+
+    CMDS = [
+
+        {
+            name: "keyup",
+            value: [0x00, 0x9a],
+            tinyg: "\n"
+        },
+        {
+            name: "reset",
+            value: [0x17, 0x8d],
+            tinyg: "None"
+        },
+        {
+            name: "sleep",
+            value: [0x00, 0x00],
+            tinyg: "None"
+        },
+        {
+            name: "stop",
+            value: [0x16, 0x8c],
+            tinyg: "!\n%\n"
+        },
+        {
+            name: "arrow1",
+            value: [0x01, 0x9b],
+            tinyg: "g28.3x0y0z0a0\n"
+        }, //Set Zero
+        {
+            name: "arrow2",
+            value: [0x09, 0x93],
+            tinyg: "g0x0y0z0a0\n"
+        }, //Go to zero
+        {
+            name: "rewind",
+            value: [0x03, 0x99],
+            tinyg: "None"
+        },
+        {
+            name: "spindle",
+            value: [0x0c, 0x96],
+            tinyg: "None"
+        },
+        {
+            name: "macro_1",
+            value: [0x0a, 0x90],
+            tinyg: "None"
+        },
+        {
+            name: "macro_2",
+            value: [0x0b, 0x91],
+            tinyg: "None"
+        },
+        {
+            name: "macro_3",
+            value: [0x05, 0x9f],
+            tinyg: "None"
+        },
+        {
+            name: "macro_6",
+            value: [0x0f, 0x95],
+            tinyg: "None"
+        },
+        {
+            name: "macro_7",
+            value: [0x10, 0x8a],
+            tinyg: "None"
+        },
+        {
+            name: "half",
+            value: [0x06, 0x9c],
+            tinyg: "None"
+        },
+        {
+            name: "zero",
+            value: [0x07, 0x9d],
+            tinyg: "g28.3"
+        },
+        {
+            name: "pause_resume",
+            value: [0x02, 0x98],
+            tinyg: "~ || !"
+        },
+        {
+            name: "probez",
+            value: [0x04, 0x9e],
+            tinyg: "g28.3z0"
+        },
+        {
+            name: "safez",
+            value: [0x08, 0x92],
+            tinyg: "g92"
+        },
+        {
+            name: "step++",
+            value: [0x0d, 0x97],
+            tinyg: "\n"
+        },
+        {
+            name: "model",
+            value: [0x0e, 0x94]
+        },
+        {
+            name: "half",
+            value: [0x03, 0x99],
+            tinyg: "None"
+        }
+    ];
+
+
+    //=====================
+    //DIAL Modes
+    //=====================
+    OFF_DIAL = 0x00;
+    X_AXIS = 0x11;
+    Y_AXIS = 0x12;
+    Z_AXIS = 0x13;
+    A_AXIS = 0x18;
+    SPINDLE_DIAL = 0x14;
+    FEED_DIAL = 0x15;
+
+    //=====================
+    //INIT COde
+    //=====================
+    this._findAndConnectPendant();
+
+};
+
+util.inherits(Xpen, EventEmitter);
+
+Xpen.prototype._findAndConnectPendant = function () {
+    var self = this;
+    try {
+        var dev = new HID.HID(this.vendorId, this.productId);
+        dev.on("data", function (data) {
+            //_dial = _getDialSetting(data[CMD_DIAL_BYTE]);
+            _packet = parseDataPacket(data);
+            if (_packet != null) {
+                //This emits the type of packet and the packet data to whatever code is
+                //listening to the xhc module
+                self.emit(_packet.type, _packet);
+            }
+        });
+
+    } catch (err) {
+        console.error("Unable to locate pendant... Check USB pendant is plugged in.");
+    }
+};
+
+var parseCommand = function (data) {
+    for (i = 0; i < CMDS.length; i++) {
+        if (data[CMD_BYTE1] == CMDS[i].value[0] && data[CMD_BYTE2] == CMDS[i].value[1]) {
+
+
+            //The Key Up command looks identical to a "jog" dial movement vs.
+            //The 2nd to last byte if its a not 0 value it makes this key_up command a jog.
+            if (data[CMD_VELOCITY] != 0x00) {
+                return ({
+                    name: "jog",
+                    value: [0x00, data[CMD_VELOCITY], 0x9a]
+                })
+            }
+            return (CMDS[i]);
+        } //eek!
+
+
+    }
+    return null;
+};
+
+var _getDialSetting = function (dialByte) {
+    //  console.warn(X_AXIS);
+    //  console.warn(dialByte);
+    switch (dialByte) {
+        case (OFF_DIAL):
+            return ("DIAL OFF");
+        case (X_AXIS):
+            return ("X");
+        case (Y_AXIS):
+            return ("Y");
+        case (Z_AXIS):
+            return ("Z");
+        case (A_AXIS):
+            return ("A");
+        case (SPINDLE_DIAL):
+            return ("SPINDLE");
+        case (FEED_DIAL):
+            return ("FEED");
+        default:
+            return ("Unknown Dial State");
+    }
+};
+
+//=================================================================================================
+//
+//                                    ---> JOGGING CODE HERE <---
+//
+//=================================================================================================
+
+
+var getJogDirectionVelocity = function (data) {
+    _velocity = data[CMD_VELOCITY];
+    //We need to figure out if this is a negative move or a positive move
+    if (_velocity > 0xaa) {
+        sign = "-";
+        _velocity = 255 - _velocity; // When rotating counter clockwise the velocity
+        //Comes in as 0xfe for 1 which we will subtract from 0xff to get a sane number
+    } else {
+        sign = "+"
+    }
+
+    return ([sign, _velocity]);
+};
+
+
+ var _resetVelocities = function(){
+    calculatedVelocity = velocityMin;
+
+}
+//Continuous is the machine will continue to jog as long as there are event dial events coming in.
+var calculateContinuousVelocity = function (vel) {
+    //build our jog command
+
+    tmpCalc = (vel * 10) * velocityMin;
+    if (tmpCalc > calculatedVelocity) {
+        calculatedVelocity = tmpCalc; //If we are moving faster than previously we will increase our speed.
+    }
+    return (calculatedVelocity);
+};
+
+
+var parseDataPacket = function (data) {
+    if (data[CMD_START_BYTE] == 0x04) { //0x04 is a constant for this device as the first byte
+        dialSetting = _getDialSetting(data[CMD_DIAL_BYTE]);
+        _tmpCmd = parseCommand(data);
+
+
+        if (_tmpCmd && dialSetting != "DIAL OFF") {
+
+            switch (_tmpCmd.name) {
+                case ("keyup"):
+                    //keyup is a weird name for this event in regards to jogging but its the same
+                    //as lifting a key after press.  So this why we must keep state on jogging.
+
+                    //If we were jogging we are in incremental mode
+                    //We need to exit this mode now that we are done jogging.
+                    if (isJogging) {
+                        isJogging = false;
+
+                        //What this does is if you are in continuous mode you will move until
+                        //you stop twisting the dial.  This will then issue a feedhold flush command.
+
+                        _resetVelocities();
+                        //We need to reset the velocity vaules to the min again.
+                        if (jogMode == "continuous") {
+                            return ({
+                                'type': 'jog',
+                                'dialSetting': dialSetting,
+                                'cmd': "jog_stop"
+                            });
+                            break;
+                        } else {
+                            return ({
+                                'type': 'jog',
+                                'dialSetting': dialSetting,
+                                'cmd': "jog_finish"
+                            });
+                            break;
+                        }
+                    }
+
+
+                case ("jog"):
+                    isJogging = true;
+                    _dirvel = getJogDirectionVelocity(data);
+                    dir = _dirvel[0];
+                    vel = _dirvel[1];
+
+                    if (jogMode == "incremental") { //Incremental Jog Sent
+                        return ({
+                            'type': 'jog',
+                            'dialSetting': dialSetting,
+                            'cmd': "jog_incremental",
+                            'dir': dir,
+                            'value': vel
+                        });
+                        break;
+
+                    } else { //Continuous Mode Jog
+                        return ({
+                            'type': 'jog',
+                            'dialSetting': dialSetting,
+                            'cmd': "jog_continuous",
+                            'dir': _dirvel[0],
+                            'value': calculateContinuousVelocity(vel)
+                        });
+                        break;
+                    }
+
+
+                case ("pause_resume"):
+                    if (isPaused) {
+                        _name = "resume";
+                    } else {
+                        _name = "feedhold";
+                    }
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _name //feedhold/resume
+                    });
+                    break;
+
+                case ("zero"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+
+                case ("half"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+                case ("spindle"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+
+                case ("rewind"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+
+                case ("reset"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+
+                case ("stop"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+
+
+                case ("arrow1"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+
+                case ("macro_1"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+
+                case ("macro_1"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+                case ("macro_2"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+
+                case ("macro_3"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+                case ("macro_6"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+                case ("macro_7"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //zero
+                    });
+                    break;
+
+                case ("safez"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //safe_z
+                    });
+                    break;
+
+                case ("probez"):
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //safe_z
+                    });
+                    break;
+
+                case ("arrow2"): //Arrow2 is, at least for now go to zero on all axis
+                    return ({
+                        'type': 'key_press',
+                        'dialSetting': dialSetting,
+                        'cmd': _tmpCmd.name //arrow2
+                    });
+                    break;
+
+                case ("step++"):
+                    console.log("Changing Step Rate for Incremental Mode");
+                    setStepDistance();
+                    console.log("\t " + getStepDistance());
+                    return ({
+                        'type': 'change',
+                        'dialSetting': dialSetting,
+                        'value': getStepDistance() //incremental value
+                    });
+                    break;
+
+                case ("model"):
+                    console.log("-----Changing Jog Modes----");
+                    if (jogMode == "incremental") {
+                        jogMode = "continuous";
+                    } else {
+                        jogMode = "incremental";
+                    }
+                    console.log("MODE: " + jogMode);
+                    break;
+
+                default:
+                    console.log("Un-Caught Case: " + _tmpCmd.name, _tmpCmd.value);
+                    break;
+
+
+            }
+
+
+        } else {
+            console.log("DIAL: " + dialSetting + " Command Code Unknown: " + data.toString('hex'));
+
+        }
+    }
+};
+
+var getStepDistance = function () {
+    return distanceTable[stepDistance];
+};
+
+var setStepDistance = function () {
+
+    //We only have 3 values in the array for distanceTable
+    //So if its 3 lets reset it back to 0
+    if (stepDistance == 3) {
+        stepDistance = 0;
+    } else {
+        stepDistance = stepDistance + 1;
+    }
+};
+
+Xpen.prototype.setUnits = function (units) {
+    var self = this;
+    if (units == self.MM) {
+        self.units = self.MM;
+    } else {
+        self.units = self.INCHES;
+    }
+    console.info("Changing Units to: " + this.units);
+
+};
+
+Xpen.prototype.test = function (b) {
+    isPaused = b; //true or false
+};
+
+
+module.exports = Xpen;
---
+++ nodechilipeppr.js
@@ -0,0 +1,139 @@
+var WebSocket = require('ws');
+var Debug = require("console-debug");
+var EventEmitter = require('events').EventEmitter;
+var Netmask = require('netmask').Netmask;
+var os = require("os");
+
+var console = new Debug({
+    uncaughtExceptionCatch: false, // Do we want to catch uncaughtExceptions?
+    consoleFilter: [], // Filter these console output types, Examples: 'LOG', 'WARN', 'ERROR', 'DEBUG', 'INFO'
+    logToFile: true, // if true, will put console output in a log file folder called 'logs'
+    logFilter: ['LOG', 'DEBUG', 'INFO'], // Examples: Filter these types to not log to file, Examples: 'LOG', 'WARN', 'ERROR', 'DEBUG', 'INFO'
+    colors: true // do we want pretty pony colors in our console output?
+});
+
+
+var NodeChilipeppr = function () {
+    EventEmitter.call(this);
+
+
+    //=====================
+    // Defines
+    //=====================
+
+
+    //=====================
+    //CMD Byte Packet Names
+    //=====================
+    CMD_BYTE2 = 5;
+
+
+};
+
+util.inherits(NodeChilipeppr, EventEmitter);
+
+NodeChilipeppr.prototype.scan = function () {
+    networks = os.networkInterfaces();
+
+    for (i = 0; i < networks.length; i++) {  //loops through interfaces
+        //for(n=0; n<networks[i].length; n++){ //loops through addresses on interfaces
+        //    if(networks[i][n].hasOwnProperty)
+        console.debug("NET: " + networks[i]);
+    }
+
+
+};
+
+//ws = new WebSocket("ws://" + argv.spjsServer + ":8989/ws");
+//
+////Handel Open Websocket
+//ws.onopen = function () {
+//    console.log("SPJS Server Connected:")
+//};
+
+//Data Events from Websocket
+//ws.onmessage = function (message) {
+//
+//
+//    if (message.hasOwnProperty("data")) {  //We always get the data attribute from SJPS
+//        try {
+//            message = JSON.parse(message.data);
+//            if (message.hasOwnProperty("Version")) {
+//                //==============
+//                //Version Posted
+//                //==============
+//                console.log("SPJS Server Version:" + message.Version);
+//                ws.send("list\n");
+//
+//
+//            } else if (message.hasOwnProperty("D")) {
+//                //==================================
+//                //Data Message Update
+//                //==================================
+//                //console.log("Status Report Update: ", message.D);
+//                var message = JSON.parse(message.D);
+//
+//
+//                //==================================
+//                //FEEDHOLD Parsing / Units / State tracking code
+//                //This code monitor for external requests to apply feed hold.
+//                //With this we can do a feedhold in chilipeppr and then resume it from our pendant
+//                //or do that in reverse.
+//                if (message.hasOwnProperty("sr")) {
+//                    if (message.sr.hasOwnProperty("stat")) {
+//                        if (message.sr.stat == 6) {//6 is Feedhold AKA pause
+//                            isPaused = true;
+//                        }
+//                        if (message.sr.stat == 5) {
+//                            isPaused = false;
+//                        }
+//                    }
+//                    //END Feed Hold Tracking Code
+//                    //==================================
+//
+//                    else if (message.sr.hasOwnProperty("unit")) {
+//                        if (message.sr.unit == MM) { //1 == mm
+//                            units = MM;
+//                        } else {                    //0 = inches
+//                            units = INCHES;
+//                        }
+//                        console.log("Changed Units to: " + units);
+//                    }
+//                    //END Units Tracking Code
+//                    //==================================
+//
+//                }
+//            } else if (message.hasOwnProperty("Commands")) {
+//                //console.log("Command Message Recvd ", message);
+//            } else if (message.hasOwnProperty("close")) {
+//                console.log("Close Message Recvd", message);
+//            } else if (message.hasOwnProperty("SerialPorts")) {
+//                //console.log("Setting Serial Ports", message);
+//                for (i = 0; i < message.SerialPorts.length; i++) {
+//                    if (message.SerialPorts[i].BufferAlgorithm == "tinyg") {
+//                        serialport = message.SerialPorts[i].Name;
+//                        console.log("\t==>Set Serial Port: " + serialport);
+//                    }
+//
+//                }
+//            }
+//            else if (message.hasOwnProperty("Error")) {
+//                console.log("Error Message:", message);
+//            }
+//            else {
+//                //Default Case when not sure of the message
+//                //console.log("RECV DATA <==:", message);
+//            }
+//        }
+//        catch (e) {
+//            //console.log("ERROR PARSING: ", e); //Just ignore the non json messages
+//        }
+//    }
+//
+//
+//    else if (message.hasOwnProperty("debug")) {
+//        console.log("Debugging Data", message);
+//    }
+//};
+
+module.exports = NodeChilipeppr;
